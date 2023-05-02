452f6105168131f3cfe8a823b0ae4c611b3fd85c
Leonardo <leonardo.mf@ufu.br>
2023-05-01 21:57:26 -0300

    n1 lab2 funcionando

--- lab2/n1.py
+++ lab2/n1.py
@@ -7,7 +7,7 @@ from unidecode import unidecode
 from glob import glob
 
 #caminho relativo dos arquivos .txt que serao lidos
-arquivos  = "txts/*.txt"
+arquivos = "txts/*.txt"
 
 #funcao que le o arquivo e cria uma lista com as palavras
 def read_file(file, wordList):
@@ -24,6 +24,7 @@ def unide(wordList):
         wordList[i] = unidecode(wordList[i])
         wordList[i] = wordList[i].lower()
         i+=1
+#remover pontuacao aqui
 
 #funcao que escreve o arquivo do vocabulario
 def write_vocabulario(vocabulario):
@@ -35,13 +36,14 @@ def write_vocabulario(vocabulario):
 #funcao que cria o vocabulario
 def create_vocabulario():
     vocabulario = []
+    listaDeArquivos = glob(arquivos)
     #le os arquivos
-    for file in glob(arquivos):
-        read_file(file, vocabulario)
+    for file in listaDeArquivos:
+        arquivo = open(file, 'rt')
+        read_file(arquivo, vocabulario)
     #remove termos duplicados
-    #fazer isso dentro do for pra salvar memoria?
     vocabulario = list(dict.fromkeys(vocabulario))
-    #usa o unidecode (usar antes ou depois de remover os termos?)
+    #unidecode no vocabulario
     unide(vocabulario)
     #coloca em ordem alfabetica
     vocabulario.sort()
@@ -51,17 +53,20 @@ def create_vocabulario():
 #funcao que le o txt do vocabulario e retorna a lista
 def read_vocabulario(path):
     vocabulario = []
-    read_file(path, vocabulario)
+    arquivoDoVocabulario = open(path, 'rt')
+    read_file(arquivoDoVocabulario, vocabulario)
     return vocabulario
 
 #funcao que cria as bag of words e printa
 def create_bow(vocabulario):
-    #lista de termos do documento
-    documento = []
+    listaDeArquivos = glob(arquivos)
     print("Bag Of Words:")
-    for file in glob(arquivos):
-        bagOfWords = []
-        read_file(file, documento)
+    for file in listaDeArquivos:
+        bagOfWords = [] #bag of words do arquivo atual 
+        documento = [] #lista de termos do arquivo atual
+        arquivoAtual = open(file, 'rt') #abre o arquivo
+        read_file(arquivoAtual, documento)
+        unide(documento)
         for termo in vocabulario:
             if documento.count(termo) == 0:
                 bagOfWords.append(0)
@@ -69,8 +74,18 @@ def create_bow(vocabulario):
                 bagOfWords.append(1)
         print(file + ":")
         print(*bagOfWords, sep=',')
-
-'''
+        
 for arquivo in glob("txts/*.txt"):
     print(arquivo)
-'''
+
+print("Criando vocabulario - done")
+#create_vocabulario()
+print("Arquivo criado - done\nLendo o vocabulario.txt")
+vocabulario = read_vocabulario("vocabulario.txt")
+print(vocabulario)
+print("lido. Printando bag of words")
+create_bow(vocabulario)
+
+
+
+
